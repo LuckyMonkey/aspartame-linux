2a68af0d1dd44312e116471cef55374247f709e2
chawee <chredden4@gmail.com>
2026-08-28 15:11:34 -0400

    complete current archiso profile contract

--- archiso/aspartame/profiledef.sh
+++ archiso/aspartame/profiledef.sh
@@ -7,9 +7,9 @@ install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux' 'uefi-x64.systemd-boot')
 arch=('x86_64')
+pacman_conf="/etc/pacman.conf"
 airootfs_image_type="squashfs"
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root/customize_airootfs.sh"]="0:0:755"
 )
-
