9d96b07c5acb59b01a120a45ba9eceafd1fddeef
chawee <chredden4@gmail.com>
2026-09-14 10:25:52 -0400

    gtk4: declare Calculate GI versions

--- packages/gtk4-calculate-activity/calculateactivity4.py
+++ packages/gtk4-calculate-activity/calculateactivity4.py
@@ -3,6 +3,10 @@
 import ast
 import operator
 
+import gi
+
+gi.require_version("Gdk", "4.0")
+gi.require_version("Gtk", "4.0")
 from gi.repository import Gdk, Gtk
 from sugar4.activity import SimpleActivity
 
